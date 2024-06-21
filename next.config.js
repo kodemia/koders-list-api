9b070a20761bb39f516ef11fa65b0a8e315f3de0
Charles Silva <charlesipn@gmail.com>
2024-06-20 19:53:56 -0600

    cors

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
